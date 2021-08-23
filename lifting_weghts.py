09ff7fb2ef25f3452c7d771b8074ece99fa8dfe7
jyshtty <56307643+jyshtty@users.noreply.github.com>
2021-08-23 09:59:57 +0530

    Update lifting_weghts.py

--- lifting_weghts.py
+++ lifting_weghts.py
@@ -1,1 +1,16 @@
+"""
+https://leetcode.com/discuss/interview-question/858129/roblox-oa-new-grad-2021
+"""
 
+
+def weightCapacity(weights, maxCapacity):
+    max_sum = set([0])
+    for weight in weights:
+        temp = set([0])
+        for i in max_sum:
+            if weight + i == maxCapacity:
+                return maxCapacity
+            elif (weight + i) < maxCapacity:
+                temp.add(weight + i)
+        max_sum.update(temp)
+    return max(max_sum)
